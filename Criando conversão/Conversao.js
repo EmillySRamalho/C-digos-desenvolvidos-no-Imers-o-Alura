ce0334e6ddfc4967b1ca0606e17a53cc06d0ec82
Emilly Santos Ramalho <emilly_bsfpb@hotmail.com>
2023-11-17 17:20:26 -0300

    codigo

---
+++ Criando conversão/Conversao.js
@@ -0,0 +1,8 @@
+var valorEmBitcoin = 64;
+var cotacaoDoBitcoin = 178.484;
+var nome = "Emilly";
+
+var valorEmReal = valorEmBitcoin * cotacaoDoBitcoin;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("Olá " + nome + "," + " o valor do Dolar está " + " R$ " + valorEmReal);
